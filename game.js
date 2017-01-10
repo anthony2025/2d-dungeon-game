17edeeb0ef08c9a4ca69d4ac8c685031050e8e8c
Anthony Ascencio <anthony2025@gmail.com>
2017-01-10 01:46:13 -0500

    tiny changes

--- game.js
+++ game.js
@@ -3,22 +3,16 @@
 const canvas = document.getElementById('gameCanvas');
 const pencil = canvas.getContext('2d');
 
-let x = 100;
-let y = 100;
-let speedx = 1;
-let speedy = 1;
-
 function drawSquare() {
   pencil.beginPath();
   pencil.rect(20, 40, 50, 50);
-  pencil.fillStyle = 'red';
+  pencil.fillStyle = 'black';
   pencil.fill();
-  pencil.strokeStyle = 'black';
-  pencil.stroke();
   pencil.closePath();
 }
 
 function render() {
   drawSquare();
 }
+
 setInterval(render, 10);
