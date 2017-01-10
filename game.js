7a8ee0086fef5ae2a715a6cc7b79800334a6c14d
Cesar Ascencio <cesarascencio97@gmail.com>
2017-01-10 02:41:02 -0500

    finished map wii

--- game.js
+++ game.js
@@ -16,17 +16,31 @@ function drawTunnels() {
   pencil.beginPath();
 
   pencil.rect(200, 0, tunnelWidth, 400);
-  pencil.rect(200, 300, 200, tunnelWidth);
+  pencil.rect(50, 300,450, tunnelWidth);
   pencil.rect(400, 200, tunnelWidth, 200);
   pencil.rect(400, 200, 250, tunnelWidth);
 
   pencil.rect(200, 0, 900, tunnelWidth);
   pencil.rect(650, 200, tunnelWidth, 350);
   pencil.rect(1050, 0, tunnelWidth, 550);
-  pencil.rect(800, 450, 400, tunnelWidth);
+  pencil.rect(800, 450, 500, tunnelWidth);
 
   pencil.rect(500, 550, 400, tunnelWidth);
-
+  pencil.rect(1300, 450, tunnelWidth, 550);
+  pencil.rect(0, 300, tunnelWidth, 450);
+   pencil.rect(400, 300, tunnelWidth, 700);
+  pencil.rect(0, 750, 750, tunnelWidth)
+  pencil.rect(450, 900, 300, tunnelWidth)
+  pencil.rect(750, 750, tunnelWidth, 450);
+  pencil.rect(800, 750, 500, tunnelWidth);
+  pencil.rect(1150, 1000, 450, tunnelWidth);
+  pencil.rect(1050, 750, tunnelWidth, 350);
+  pencil.rect(850, 50, tunnelWidth, 200);
+  pencil.rect(850, 250, 450, tunnelWidth);
+  pencil.rect(1200, 250, tunnelWidth, 200);
+  pencil.rect(800, 550, tunnelWidth, 200);
+  pencil.rect(1050, 550, tunnelWidth, 200);
+  pencil.rect(400, 1200, 700, 400)
   pencil.fillStyle = '#000';
   pencil.fill();
   pencil.closePath();
